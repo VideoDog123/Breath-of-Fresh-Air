dd55c24bf6bab229bc1a16efad2371b577e1a0c7
VideoDog123 <jacksonrodosta@gmail.com>
2026-03-13 03:49:57 -0700

    Update and rename main test.py to main.py
    Frist functional version of the main driver file

--- main test.py
+++
@@ -1,9 +0,0 @@
-
-
-# #from myFunctions import room1, room2, room3
-#
-# def main():
-#     room1()
-#
-# main()
-print("project upload test placeholder")
---
+++ main.py
@@ -0,0 +1,27 @@
+#Jackson Rodosta
+#Started: 3/5/2026
+#Last updated: 3/13/2026
+#BFA text adventure game. Takes user input to describe surroundings and move between rooms. The goal is to reach the surface.
+#This is the main file that calls the necessary functions in order
+
+from functions import act1, act2
+
+RED = '\033[91m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+BLUE = '\033[94m'
+MAGENTA = '\033[95m'
+CYAN = '\033[96m'
+WHITE = '\033[97m'
+RESET = '\033[0m'
+# color definitions for quick reference
+
+def main():
+# displays the title and starts act1, then act2, act3 is handled by room2_cave
+    print(f"{BLUE}Welcome to Breath of Fresh Air\n")
+    act1()
+    act2()
+
+if __name__ == '__main__':
+    main()
+  
